dd5c4e918877e3d0171562b36b5f76140dcaec4b
Hanna Lee <mani21na@gmail.com>
2019-08-16 22:39:07 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,34 +1,32 @@
-def first_challenge
-   epic_tragedy = 
-   {
-        montague: {
-                patriarch: {
-                        name: "Lord Motague",
-                        age: "53"
-                  },
-                matrisrch: {
-                        name: "Lady Montague",
-                        age: "54"
-                },
-                hero: {
-                        name: "Romeo",
-                        age: "15"
-                },
-                hero_friend: [
-                      {
-                        name: "Benvolio",
-                        age: "17",
-                        attitude: "worried"
-                      },
-                      {
-                        name: "Mercutio",
-                        age: "18",
-                        attitude: "hot-headed"
-                        }
-                ]
-        },
-        capulet: {
-                :patriarch => {
+epic_tragedy = {
+  :montague => {
+    :patriarch => {  
+      :name => "Lord Montague",
+      :age => "53"
+  },
+    :matriarch => {
+      :name => "Lady Montague",
+      :age => "54"
+  },
+    :hero => {
+      :name => "Romeo",
+      :age => "15"
+  },
+    :hero_friends => [
+      {
+         :name => "Benvolio",
+         :age => "17",
+         :attitude => "worried"
+      },
+      {
+        :name => "Mercutio",
+        :age => "18",
+        :attitude => "hot-headed"
+      }
+    ]
+  },
+  :capulet => {
+    :patriarch => {
       :name => "Lord Capulet",
       :age => "50"
     },
@@ -53,9 +51,5 @@ def first_challenge
         :attitude => "worried"
       }
     ]
-        }
-   }
-   
-     
-   
-end
+  }
+}
