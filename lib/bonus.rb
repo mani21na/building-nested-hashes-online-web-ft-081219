5df57aa2a8e1312f47d1368050e0ee5d5b7d397b
Hanna Lee <mani21na@gmail.com>
2019-08-16 23:01:14 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -23,7 +23,7 @@ def bonus
   #code your solution here:
 
   epic_tragedy[:montague][:patriarch][:name] = "Michael Jordan"
- # epic_tragedy[:montague][:hero][:status] = "dead"
+  epic_tragedy[:montague][:hero][:status] = "dead"
   epic_tragedy[:capulet][:heroine][:status] = "dead"
   
   puts epic_tragedy
