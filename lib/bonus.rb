2f60f81f5222fffdf9f0472d08ea35639ce12c57
Hanna Lee <mani21na@gmail.com>
2019-08-16 23:00:42 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -24,6 +24,7 @@ def bonus
 
   epic_tragedy[:montague][:patriarch][:name] = "Michael Jordan"
   epic_tragedy[:montague][:hero][:status] = "dead"
+  epic_tragedy[:capulet][:heroine][:status] = "dead"
   
   puts epic_tragedy
 
